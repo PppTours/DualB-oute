e27f35ac85884b4b9f02523a8273a224b5489db8
BOSSY Louis <louis.bossy85@gmail.com>
2024-12-06 00:11:24 +0100

    QR-code V3

--- qr_code/qr_code.py
+++ qr_code/qr_code.py
@@ -3,8 +3,9 @@ from PIL import Image
 from random import randint
 import urllib.parse
 
-IMG_DIM = 500
-poke_colors = ((255,255,20,255),(150,150,255,255),(0,255,0,255),(220,200,255,255))
+IMG_DIM = 100
+poke_colors = ((220,220,20,255),(150,150,255,255),(0,255,0,255),(220,200,255,255),(255,80,60,255),(255,150,30,255),(20,240,240,255),
+               (255,100,255,255), (200,0,255,255), (0,180,255,255))
 
 def get_code(data : str):
     encoded_data = urllib.parse.quote(data)  # Encodage des caractères spéciaux
@@ -23,19 +24,21 @@ def get_pixels(img : Image):
              pixdata.append(pix)
      return pixdata
 
-def main(data : str):
+def generate_QR(data : str):
      
      while len(data)<174:
           data += ' '
 
      get_code(data)
 
-     poke=randint(1,4)
+     poke = randint(1,10)
+     bg = Image.open("qr_code/bg.jpg")
      code_img = Image.open("qr_code/qrcode.png", "r", None).convert("RGBA").convert("RGBA").resize((IMG_DIM,IMG_DIM), 1)
      poke_img = Image.open("qr_code/shadows/"+str(poke)+".png", "r", None).resize((IMG_DIM,IMG_DIM), 1)
 
      code_pix = get_pixels(code_img)
      poke_pix = get_pixels(poke_img)
+
      new_pix = []
 
      for i in range(IMG_DIM*IMG_DIM):
@@ -53,4 +56,6 @@ def main(data : str):
      new_img=Image.new(mode='RGBA', size=(IMG_DIM,IMG_DIM))
      new_img.putdata(new_pix)
 
-     new_img.show()
+     bg.paste(new_img, (35,35))
+     
+     bg.show()
