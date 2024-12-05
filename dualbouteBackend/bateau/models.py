e489672288d78386b6ba5274080616a994384b63
matteo langlois <langlois.matteo49@gmail.com>
2024-12-05 18:38:05 +0100

    Add the models for the backend

--- dualbouteBackend/bateau/models.py
+++ dualbouteBackend/bateau/models.py
@@ -2,19 +2,14 @@ from django.db import models
 
 # Create your models here.
 class PartieDuCorps(models.Model):
-    nom = models.CharField(max_length=100)
-    svg_id = models.CharField(max_length=100)
+    nom = models.CharField(max_length=100, null=True, blank=True)
+    svg_id = models.CharField(max_length=100, null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(default="")
+    image = models.ImageField(upload_to='media/', null=True, blank=True)
 
     def __str__(self):
         return self.nom
-
-class EcoData(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='media/')
-
-    def __str__(self):
-        return self.title
 
 
 
--- dualbouteBackend/bateau/views.py
+++ dualbouteBackend/bateau/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import PartieDuCorps
+from .serializers import PartieDuCorpsSerializer
 
 # Create your views here.
+class PartieDuCorpsViewSet(viewsets.ModelViewSet):
+    queryset = PartieDuCorps.objects.all()
+    serializer_class = PartieDuCorpsSerializer
