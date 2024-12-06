7d9b2f21e4d4d32382d15efa56aacbc25de0f15f
matteo langlois <langlois.matteo49@gmail.com>
2024-12-06 02:50:35 +0100

    WORKS

--- dualboute-frontend/src/app/poc/poc.component.ts
+++ dualboute-frontend/src/app/poc/poc.component.ts
@@ -1,32 +1,66 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, AfterViewInit } from '@angular/core';
 import { PocService } from '../shared/service/pocservice.service';
+import { NgIf, NgOptimizedImage } from '@angular/common';
+import { SvgComponent } from '../feature/svg/svg.component';
 
 @Component({
   selector: 'app-poc',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NgIf,
+    SvgComponent
+  ],
   templateUrl: './poc.component.html',
-  styleUrl: './poc.component.css'
+  styleUrls: ['./poc.component.css']
 })
-export class PocComponent implements OnInit {
-  data: any;  
+export class PocComponent implements OnInit, AfterViewInit {
   show = false;
   title: string = "";
-  description: string = ""
-  media: string = ""
+  description: string = "";
+  media: string = "";
+  data: any;
+  afterViewInit = false;
+  afterInit = false;
 
-  @ViewChild('informationDiv', { static: false }) informationDiv!: ElementRef<HTMLDivElement>;
+  @ViewChild('svgElement', { static: false, read: ElementRef }) svgElement!: ElementRef<SVGElement>;
 
   constructor(private pocService: PocService, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    this.pocService.getAll().subscribe(
-      (response) => {
-        this.data = response; 
-      },
-      (error) => {
-        console.error('Error loading data', error); 
+    this.pocService.getAll().subscribe((data: any) => {
+      this.data = data;
+      this.afterInit = true;
+      this.addClickListenersToSvgElements()
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const svgElement = document.querySelector('svg');
+    if (svgElement) {
+      this.svgElement = new ElementRef(svgElement);
+    } else {
+      console.error('No SVG element found');
+    }
+    this.afterViewInit = true;
+    this.addClickListenersToSvgElements();
+  }
+
+  addClickListenersToSvgElements(): void {
+    if (!this.afterViewInit || !this.afterInit) {
+      return;
+    }
+    if (this.svgElement && this.data) {
+      const svg = this.svgElement.nativeElement;
+      for (let item of this.data) {
+        const elements = svg.querySelector(`#${item.svg_id}`);
+        if (elements) {
+          elements.addEventListener('click', (event) => {
+            this.loadInformations(event);
+          });
+        }
+
       }
-    );
+    }
   }
 
   closeInformation(): void {
@@ -41,9 +75,11 @@ export class PocComponent implements OnInit {
   loadInformations(event: Event): void {
     if (event.target !== null) {
       const element = event.target as HTMLElement;
+      console.log(this.data.length);
       for (let i = 0; i < this.data.length; i++) {
         if (this.data[i].svg_id == element.id) {
-          this.show = true; 
+          console.log(this.data[i]);
+          this.show = true;
           this.title = this.data[i].title;
           this.description = this.data[i].description;
           this.media = this.data[i].image;
