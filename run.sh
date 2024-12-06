13e3d3d51479d077a04bf0055028a6e2f7841aa7
Quentin Horgues <quentin.horgues@outlook.fr>
2024-12-06 02:32:16 +0100

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Démarrer le serveur Angular
-gnome-terminal -- bash -c "cd ./dualboute-frontend && npm start; exec bash"
+cd ./dualboute-frontend && npm start; exec bash
 
 # Démarrer le serveur Django
-gnome-terminal -- bash -c "cd ./dualbouteBackend && .venv/bin/python manage.py runserver; exec bash"
+cd ./dualbouteBackend && .venv/bin/python manage.py runserver; exec bash
 
 # Attendre que les serveurs soient arrêtés
 read -p "Appuyez sur une touche pour continuer..."
